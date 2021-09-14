5cf76e15e2ffb6e9a0aeea76a566d24a933e2971
Artem Tsolov <71283039+tsolovart@users.noreply.github.com>
2021-09-14 11:26:32 +0300

    Initial

---
+++ toDoList/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  toDoList
+//
+//  Created by Zaoksky on 13.09.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ toDoList/Controller/TasksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TasksViewController.swift
+//  toDoList
+//
+//  Created by Zaoksky on 14.09.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
